a7a216387a6022a4daad40a2cd2360ab84c525c9
mutiulahi <tesleemolamilekan902@gmail.com>
2022-09-02 00:19:57 +0100

    fixed cv

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -10,8 +10,8 @@ use App\Models\Work_experience;
 use App\Models\Skill;
 use App\Models\Social;
 // use Barryvdh\DomPDF\Facade\PDF;
-use Barryvdh\DomPDF\Facade as PDF;
-// use PDF;
+// use Barryvdh\DomPDF\Facade as PDF;
+use PDF;
 use Illuminate\Support\Facades\Storage;
 
 
