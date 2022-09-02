c44c63ddd03c4d421cc6aa7cc7ef5fcc59d2762d
mutiulahi <tesleemolamilekan902@gmail.com>
2022-09-02 09:06:35 +0100

    fixed cv

--- config/app.php
+++ config/app.php
@@ -210,7 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'pdf' => Barryvdh\DomPDF\Facade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ])->toArray(),
 
 ];
